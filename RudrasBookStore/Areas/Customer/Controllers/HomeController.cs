41059761fa34ba9c8ea4bbbe494663a77b2abbc1
patelrudra10 <RP119@myscc.ca>
2022-03-26 13:39:54 -0400

    Done with part 01

--- RudrasBookStore/Areas/Customer/Controllers/HomeController.cs
+++ RudrasBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace RudrasBookStore.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
